1db9d5065ef17d10339e782a73437b9ddaa2235d
mars2nico <13172812+mars2nico@users.noreply.github.com>
2024-03-18 06:30:06 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use iced::widget::{button, column, container, horizontal_space, row, text, text_editor};
-use iced::{executor, Application, Command, Element, Length, Settings, Theme};
-
+use iced::highlighter::{self, Highlighter};
+use iced::theme;
+use iced::widget::{button, column, container, horizontal_space, row, text, text_editor, tooltip};
+use iced::{executor, Application, Command, Element, Font, Length, Settings, Theme};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 fn main() -> iced::Result {
-    Editor::run(Settings::default())
+    Editor::run(Settings {
+        default_font: Font::MONOSPACE,
+        #[rustfmt::skip]
+        fonts: std::vec::Vec::from([
+            include_bytes!("../fonts/editor-icons.ttf")
+            .as_slice() // なぜ &[u8, N] から直接 Cow<'_, [u8]> に into できず、as_slice が必要なのか？
+            .into(),
+        ]),
+        ..Settings::default()
+    })
 }
 
 struct Editor {
     path: Option<PathBuf>,
     content: text_editor::Content,
     error: Option<Error>,
+    theme: highlighter::Theme,
+    is_dirty: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -22,6 +34,7 @@ enum Message {
     FileOpened(Result<(PathBuf, Arc<String>), Error>),
     Save,
     FileSaved(Result<PathBuf, Error>),
+    SwitchTheme,
 }
 
 impl Application for Editor {
@@ -36,6 +49,8 @@ impl Application for Editor {
                 path: None,
                 content: text_editor::Content::new(),
                 error: None,
+                theme: highlighter::Theme::SolarizedDark,
+                is_dirty: true,
             },
             Command::perform(load_file(default_file()), Message::FileOpened),
         )
@@ -48,19 +63,22 @@ impl Application for Editor {
     fn update(&mut self, message: Self::Message) -> Command<Message> {
         match message {
             Message::Edit(action) => {
-                self.content.edit(action);
+                self.is_dirty = self.is_dirty || action.is_edit();
                 self.error = None;
+                self.content.edit(action);
                 Command::none()
             }
             Message::New => {
                 self.path = None;
                 self.content = text_editor::Content::new();
+                self.is_dirty = true;
                 Command::none()
             }
             Message::Open => Command::perform(pick_file(), Message::FileOpened),
             Message::FileOpened(Ok((path, content))) => {
                 self.path = Some(path);
                 self.content = text_editor::Content::with(&content);
+                self.is_dirty = false;
                 Command::none()
             }
             Message::FileOpened(Err(error)) => {
@@ -74,10 +92,19 @@ impl Application for Editor {
             }
             Message::FileSaved(Ok(path)) => {
                 self.path = Some(path);
+                self.is_dirty = false;
                 Command::none()
             }
             Message::FileSaved(Err(error)) => {
                 self.error = Some(error);
+                Command::none()
+            }
+            Message::SwitchTheme => {
+                self.theme = if self.theme.is_dark() {
+                    highlighter::Theme::InspiredGitHub
+                } else {
+                    highlighter::Theme::SolarizedDark
+                };
                 Command::none()
             }
         }
@@ -85,11 +112,34 @@ impl Application for Editor {
 
     fn view(&self) -> Element<'_, Self::Message> {
         let controls = row![
-            button("New").on_press(Message::New),
-            button("Open").on_press(Message::Open),
-            button("Save").on_press(Message::Save),
+            action(new_icon(), "New file", Some(Message::New)),
+            action(open_icon(), "Open file", Some(Message::Open)),
+            action(
+                save_icon(),
+                "Save file",
+                self.is_dirty.then_some(Message::Save)
+            ),
+            horizontal_space(Length::Fill),
+            action(
+                theme_icon(self.theme.is_dark()),
+                "Switch theme",
+                Some(Message::SwitchTheme)
+            ),
         ];
-        let input = text_editor(&self.content).on_edit(Message::Edit);
+        let input = text_editor(&self.content)
+            .on_edit(Message::Edit)
+            .highlight::<Highlighter>(
+                highlighter::Settings {
+                    theme: self.theme,
+                    extension: self
+                        .path
+                        .as_ref()
+                        .and_then(|path| path.extension()?.to_str())
+                        .unwrap_or("js")
+                        .to_string(),
+                },
+                |highlight, _theme| highlight.to_format(),
+            );
 
         let status_bar = {
             let status = if let Some(Error::IOFailed(error)) = &self.error {
@@ -116,8 +166,60 @@ impl Application for Editor {
     }
 
     fn theme(&self) -> Theme {
-        Theme::Dark
+        if self.theme.is_dark() {
+            Theme::Dark
+        } else {
+            Theme::Light
+        }
     }
+}
+
+fn action<'a>(
+    content: Element<'a, Message>,
+    label: &str,
+    on_press: Option<Message>,
+) -> Element<'a, Message> {
+    let is_disabled = on_press.is_none();
+    tooltip(
+        button(container(content).width(30).center_x())
+            .on_press_maybe(on_press)
+            .padding([5, 10])
+            .style(if is_disabled {
+                theme::Button::Secondary
+            } else {
+                theme::Button::Primary
+            }),
+        label,
+        tooltip::Position::FollowCursor,
+    )
+    .style(theme::Container::Box)
+    .into()
+}
+
+fn new_icon<'a>() -> Element<'a, Message> {
+    icon('\u{E800}')
+}
+
+fn open_icon<'a>() -> Element<'a, Message> {
+    icon('\u{F115}')
+}
+
+fn save_icon<'a>() -> Element<'a, Message> {
+    icon('\u{E801}')
+}
+
+fn theme_icon<'a>(is_dark: bool) -> Element<'a, Message> {
+    if is_dark {
+        icon('\u{E802}')
+    } else {
+        icon('\u{F185}')
+    }
+}
+
+fn icon<'a, Message>(codepoint: char) -> Element<'a, Message> {
+    const ICON_FONT: Font = Font::with_name("editor-icons");
+
+    text(codepoint).font(ICON_FONT).into()
 }
 
 fn default_file() -> PathBuf {
@@ -137,7 +239,7 @@ async fn pick_file() -> Result<(PathBuf, Arc<String>), Error> {
 async fn load_file(path: PathBuf) -> Result<(PathBuf, Arc<String>), Error> {
     let read_result = tokio::fs::read_to_string(&path)
         .await
-        .map(Arc::new) // スレッド間で
+        .map(Arc::new) // パフォーマンスの問題上、ファイルの内容をCloneすべきでないためArcで囲む https://youtu.be/gcBJ7cPSALo?t=1598
         .map_err(|error| error.kind())
         .map_err(Error::IOFailed);
 
